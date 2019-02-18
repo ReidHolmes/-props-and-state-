593f3dc6ea8215bf7b3ddc0e7257ba51f1ebf9a9
ReidHolmes <name.reidholmes@gmail.com>
2019-02-18 11:19:34 +0800

    props and state

--- App.js
+++ App.js
@@ -7,44 +7,44 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
+
+
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Text, View} from 'react-native';
+class Blink extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { isShowingText: true };
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+        // 每1000毫秒对showText状态做一次取反操作
+        setInterval(() => {
+            this.setState(previousState => {
+                return { isShowingText: !previousState.isShowingText };
+            });
+        }, 1000);
+    }
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+    render() {
+        // 根据当前showText的值决定是否显示text内容
+        if (!this.state.isShowingText) {
+            return null;
+        }
+
+        return (
+            <Text>{this.props.text}</Text>
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+export default class BlinkApp extends Component {
+    render() {
+        return (
+            <View>
+                <Blink text='I love to blink' />
+                <Blink text='Yes blinking is so great' />
+                <Blink text='Why did they ever take this out of HTML' />
+                <Blink text='Look at me look at me look at me' />
+            </View>
+        );
+    }
+}
